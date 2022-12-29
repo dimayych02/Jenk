fe2474584c872d55af74210637f714d35bd0d52c
Дмитрий Кирилин <106656570+dimayych02@users.noreply.github.com>
2022-12-29 13:51:25 +0400

    Update and rename PageObjectPattern.java to PageObjectPatternTest.java

--- src/test/java/PageObjectPatternTest.java
+++ src/test/java/PageObjectPatternTest.java
@@ -1,9 +1,12 @@
-import org.testng.IRetryAnalyzer;
-import org.testng.ITestResult;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Step;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+@Listeners(TestListener.class)
+public class PageObjectPatternTest   {
 
-public class PageObjectPattern  implements IRetryAnalyzer {
     int count =0;
     int maxcount =4;
     public final static String GooglePath = "https://www.google.com/";
@@ -11,13 +14,13 @@ public class PageObjectPattern  implements IRetryAnalyzer {
     public final static String link = "//a[@href=\"https://account.mail.ru/login\"]";
     public final static String Search ="//input[@class=\"gLFyf\"]";
     public final static String LoginPath = "//input[@name=\"username\"]";
-    public final static String LoginData = "taskmail123456";
+    public final static String LoginData = "askmail123456";
     public final static String PasswordPath ="//input[@name=\"password\"]";
     public final static String PasswordData ="vsemprivet@12345671245etedgvg";
     public final static String Except ="//div[@class=\"ph-project-promo-close-icon__container svelte-m7oyyo\"]";
     public final static String SendMessagePath ="//span[@class=\"compose-button__txt\"]";
     public final static String EmailXpath ="//input[@style=\"width: 12px;\"]";
-    public final static String Email = "dokuchaev_av@tkbbank.ru";
+    public final static String Email = "dim_dimyych@mail.ru";
     public final static String XpathMessage = "//div[@aria-multiline=\"true\"]//child::div";
     public final static String Message = "Добрый день\n Простейший автотест готов\n Кирилин Дмитрий Дмитриевич\n ссылка на код:https://github.com/dimayych02/ss.git";
 
@@ -27,40 +30,48 @@ public class PageObjectPattern  implements IRetryAnalyzer {
     ru Test = new ru();
 
     @Test
+    @Severity(SeverityLevel.NORMAL)
+    @Step("SettingDriver")
     public void test1(){
         Test.SettingDriver();
     }
-
     @Test
+    @Severity(SeverityLevel.MINOR)
+    @Step("OpenLink")
     public void test2(){
         Test.OpenDriver(GooglePath);
     }
 
     @Test
+    @Severity(SeverityLevel.NORMAL)
+    @Step("OpenEmail")
     public void test3(){
         Test.OpenMail(Search,mail,link);
     }
-
     @Test
+    @Severity(SeverityLevel.BLOCKER)
+    @Step("Verification")
     public void test4() {
 
         Test.LogIn(LoginPath,LoginData,PasswordPath,PasswordData);
     }
-
-    @Test(retryAnalyzer=PageObjectPattern.class)
+    @Test
+    @Severity(SeverityLevel.BLOCKER)
+    @Step("sendingMessage")
     public void test5(){
 
         Test.SendMessage(Except,SendMessagePath,EmailXpath,Email,XpathMessage,Message,v);
     }
+    /*@AfterEach
+    @Step("CloseDriver")
+    public void test6(){
+        Allure.getLifecycle().addAttachment("screenshot","image/png","png"
+                ,((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)
+        );
+        driver.close();
+        driver.quit();
+    }*/
 
-    @Override
-    public boolean retry(ITestResult result) {
-        if (!result.isSuccess()) {
-            if (count < maxcount) {
-                count++;
-                return true;
-            }
-        }
-        return false;
-    }
+
+
 }
